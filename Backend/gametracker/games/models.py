c87c03d93edd7e4fd107b601aff6ffb6f1b08a15
colewiltse <cole.wiltse@mail.utoronto.ca>
2026-02-18 17:38:43 -0500

    adding game model

--- Backend/gametracker/games/models.py
+++ Backend/gametracker/games/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
+
+CONSOLES = (
+    ('ps5', 'Playstation 5'),
+    ('ps4', 'Playstation 4'),
+    ('ps3', 'Playstation 3'),
+    ('ps2', 'Playstation 2'),
+    ('ps1', 'Playstation 1'),
+    ('xsx', 'Xbox Series X'),
+    ('xss', 'Xbox Series S'),
+    ('xb1', 'Xbox One'),
+    ('xb360', 'Xbox 360'),
+    ('xb', 'Xbox'),
+    ('ns2', 'Switch 2'),
+    ('nsw', 'Switch'),
+    ('wiiu', 'Wii U'),
+    ('wii', 'Wii'),
+    ('gcn', 'GameCube'),
+    ('n64', 'Nintendo 64'),
+    ('snes', 'SNES'),
+    ('nes', 'NES'),
+    ('pc', 'PC'),
+    ('other', 'Other'),
+)
+
 # Create your models here.
+class Game(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=50)
+    console = models.CharField(max_length=30, choices=CONSOLES)
+    description = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.title + " - " + self.console
