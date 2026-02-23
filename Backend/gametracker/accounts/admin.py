8522bd6c766f9d7c8d619001e48164671ed099e2
colewiltse <cole.wiltse@mail.utoronto.ca>
2026-02-23 13:54:43 -0500

    Finishing up login system

--- Backend/gametracker/accounts/admin.py
+++ Backend/gametracker/accounts/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from models import GameTrackerUser
+from .models import GameTrackerUser
 # Register your models here.
 admin.site.register(GameTrackerUser)
--- Backend/gametracker/gametracker/settings.py
+++ Backend/gametracker/gametracker/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,9 +39,25 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework_simplejwt',
     'games',
     'accounts',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES' : [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES' : (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    
+    ),
+    
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME' : timedelta(days=1),
+}
 
 AUTH_USER_MODEL = 'accounts.GameTrackerUser'
 
