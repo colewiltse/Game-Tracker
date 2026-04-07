7ffbfecd20f09ede787c8d735f3da0e3a525f40b
colewiltse <cole.wiltse@mail.utoronto.ca>
2026-04-07 00:43:34 -0400

    Trying another change to db to fix bugs

--- Backend/gametracker/gametracker/settings.py
+++ Backend/gametracker/gametracker/settings.py
@@ -126,10 +126,10 @@ WSGI_APPLICATION = 'gametracker.wsgi.application'
 #     }
 # Replace the SQLite DATABASES configuration with PostgreSQL:
 DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
